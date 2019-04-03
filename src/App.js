aad89afb497559c7889b02715071d841a7538968
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-04-03 23:56:37 +0300

    Added Layout component which will be used like markup component. Create simple HOC. Added BurgerBuilder class based component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Start the project</h1>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
