907f32600db99cdd50ebf4a3a9b489c37b6bd92b
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-04-19 15:02:23 +0300

    create styles for orders in orders_busket page

--- src/components/ForPages/OrdersForBusket/Orders.js
+++ src/components/ForPages/OrdersForBusket/Orders.js
@@ -1,10 +1,35 @@
 import React from "react";
 
+import classes from "./Orders.module.css";
+
 const OrdersForBusket = () => {
   return (
-    <div>
-      <p>Salad: 2</p>
-      <p>Price: 2.44</p>
+    <div className={classes.Orders}>
+      <h3>
+        Order <span>№1</span>
+      </h3>
+      <div className={classes.Ingredients}>
+        <h4>Ingredients: </h4>
+
+        <ul>
+          <li>
+            Salad <span>4</span>
+          </li>
+          <li>
+            Becon <span>4</span>
+          </li>
+          <li>
+            Cheese <span>1</span>
+          </li>
+          <li>
+            Meat <span>2</span>
+          </li>
+        </ul>
+      </div>
+
+      <p className={classes.Price}>
+        Price: &#36;<span>2.44</span>
+      </p>
     </div>
   );
 };
